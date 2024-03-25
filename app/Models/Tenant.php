3e590d1de4ea621bf4084634be24325f9de8e179
jasmineknsh <jasminekinasih10@gmail.com>
2024-03-25 10:48:17 +0800

    update model

--- app/Models/Tenant.php
+++ app/Models/Tenant.php
@@ -4,8 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Tenant extends Model
 {
     use HasFactory;
+
+    protected $table = 'tenants';
+    protected $guarded = [];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function product() : HasMany
+    {
+        return $this->hasMany(Product::class);
+    }
+
+    
 }
