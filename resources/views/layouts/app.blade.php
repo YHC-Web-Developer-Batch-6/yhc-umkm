b4c7fd5622d90667e0678259da13e610c42da155
matzgans <rahmatlahay84@gmail.com>
2024-03-25 12:55:45 +0800

    commit_mat_footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,17 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
 
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>YHC UMKM</title>
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>YHC UMKM</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
 
-    <body>
-        @include('layouts.partial.navbar')
-        @yield('content')
-    </body>
+<body>
+    @include('layouts.partial.navbar')
+    @yield('content')
+    @include('layouts.partial.footer')
+</body>
 
 </html>
